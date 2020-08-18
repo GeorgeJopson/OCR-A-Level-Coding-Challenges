db216177482ff8484c800cf0b335f35cb0a15776
George Jopson <georgejopson@gmail.com>
2020-08-18 11:17:10 +0100

    Add Palindromes challenge

---
+++ 20-Palindromes/Palindromes.py
@@ -0,0 +1,8 @@
+from math import floor
+word=input("Input a word:\n").lower()
+palindrome=True
+for letter in range(floor(len(word)/2)):
+    if(word[letter]!=word[-(letter+1)]):
+        palindrome=False
+statement="is" if palindrome==True else "is not"
+print(word.capitalize()+" "+statement+" a palindrome.")
