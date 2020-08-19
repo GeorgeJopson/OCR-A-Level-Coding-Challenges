be47845982d007d8d4a3c4287efeed9b3ec6d723
George Jopson <georgejopson@gmail.com>
2020-08-19 15:35:14 +0100

    Add Mor-se Coding challenge

---
+++ 33-Mor-se Coding (Extension Available)/Mor-se Coding.py
@@ -0,0 +1,35 @@
+morseCode={
+    'a':'.-',
+    "b":"-...",
+    "c":"-.-.",
+    "d":"-..",
+    "e":".",
+    "f":"..-.",
+    "g":"--.",
+    "h":"....",
+    "i":"..",
+    "j":".---",
+    "k":"-.-",
+    "l":".-..",
+    "m":"--",
+    "n":"-.",
+    "o":"---",
+    "p":".--.",
+    "q":"--.-",
+    "r":".-.",
+    "s":"...",
+    "t":"-",
+    "u":"..-",
+    "v":"...-",
+    "w":".--",
+    "x":"-..-",
+    "y":"-.--",
+    "z":"--..",
+    " ":"|"
+    }
+message=input("Input a message: ")
+encodedMessage=[]
+for letter in message:
+    encodedMessage.append(morseCode.get(letter.lower()))
+encodedMessage=" ".join(encodedMessage)
+print(encodedMessage)
