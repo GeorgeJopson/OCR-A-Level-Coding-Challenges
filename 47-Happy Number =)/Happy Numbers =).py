62dfe93984613f447aafc7b594e8a6d007b8a8a5
George Jopson <georgejopson@gmail.com>
2020-08-31 13:10:09 +0100

    Add Happy Numbers =) challenge

---
+++ 47-Happy Number =)/Happy Numbers =).py
@@ -0,0 +1,17 @@
+#Exact copy of challenge 9 (Happy Numbers)
+def isHappyNum(number):
+    previousNums=[]
+    while True:
+        number=sum(map(lambda x:int(x)*int(x), str(number)))
+        if(number in previousNums):
+            return False
+        if(number==1):
+            return True
+        previousNums.append(number)
+happyNumList=[]
+counter=0
+while len(happyNumList)<8:
+    if(isHappyNum(counter)):
+        happyNumList.append(counter)
+    counter+=1
+print(happyNumList)
