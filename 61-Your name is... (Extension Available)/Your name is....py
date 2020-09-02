71f56017b2ce6452dc952db55e19bbd6f8e8e390
George Jopson <georgejopson@gmail.com>
2020-09-02 17:24:56 +0100

    Add Your name is... challenge

---
+++ 61-Your name is... (Extension Available)/Your name is....py
@@ -0,0 +1,4 @@
+name=input("What is your name?\n")
+age=input("What is your age?\n")
+form=input("What is your form?\n")
+print("Your name is {0}, you are {1} years old, and you are in form {2}.".format(name,age,form))
