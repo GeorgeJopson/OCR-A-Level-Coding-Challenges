b8959ecdaa5e866b2ae603dc2f14f90a04a25282
George Jopson <georgejopson@gmail.com>
2020-09-04 11:47:19 +0100

    Add Printer problems challenge

---
+++ 79-Printer Problems/Printer Problems.py
@@ -0,0 +1,9 @@
+#This task doesn't actually have a question or really any task attached to it.
+#All it has is a bunch of statements about this dude cutting peices of paper in half.
+#This means I can't do anything for it.
+#Please have a cat in instead:
+
+#  ^~^  ,
+# ('Y') )
+# /   \/ 
+#(\|||/)
